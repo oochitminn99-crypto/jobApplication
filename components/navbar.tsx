be7d30fc7bbd26996c4c760f61951616af0735e3
oochitminn99-crypto <oochitminn99@gmail.com>
2026-08-06 21:41:59 +0630

    add sign in

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,17 @@
+"use client";
+
 import { Briefcase } from "lucide-react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button"
+import { getSession, signOut } from "@/lib/auth/auth";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { Avatar, AvatarFallback } from "./ui/avatar";
+import SignOutButton from "./sign-out-btn";
+import { useSession } from "@/lib/auth/auth-client";
 
 export default function Navbar() {
+    const {data: session} = useSession()
+
     return (
         <nav className="border-b border-gray-200 bg-white">
             <div className="container mx-auto flex h-16 items-center px-4 justify-between">
@@ -13,16 +22,54 @@ export default function Navbar() {
                 </Link>
 
                 <div className="flex items-center gap-4">
-                    <Link href="/sign-in">
-                        <Button variant="ghost" className="text-white bg-mauve-300 hover:font-bold hover:bg-mauve-300 italic">
-                            Log In
-                        </Button>
-                    </Link>
-                    <Link href="/sign-up">
-                        <Button className="bg-pink-400 hover:bg-pink-800 hover:font-bold">
-                            Start for free
-                        </Button>
-                    </Link>
+                    {session?.user ? (
+                        <>
+                            <Link href="/dashboard">
+                                <Button variant="ghost"
+                                className="text-gray-700 hover:text-black">
+                                    Dashboard
+                                </Button>
+                            </Link>
+                            <DropdownMenu>
+                                <DropdownMenuTrigger>
+                                    <Button variant="ghost">
+                                        <Avatar>
+                                            <AvatarFallback className="bg-pink-400 text-white font-bold">
+                                                {session.user.name[0].toUpperCase()}
+                                            </AvatarFallback>
+                                        </Avatar>
+                                    </Button>
+                                </DropdownMenuTrigger>
+
+                                <DropdownMenuContent>
+                                    <DropdownMenuLabel>
+                                        <div>
+                                            <p>
+                                                {session.user.name}
+                                            </p>
+                                            <p>
+                                                {session.user.email}
+                                            </p>
+                                        </div>
+                                    </DropdownMenuLabel>
+                                    <SignOutButton />
+                                </DropdownMenuContent>
+                            </DropdownMenu>
+                        </>
+                    ) : (
+                        <>
+                            <Link href="/sign-in">
+                                <Button variant="ghost" className="text-white bg-mauve-300 hover:font-bold hover:bg-mauve-300 italic">
+                                    Log In
+                                </Button>
+                            </Link>
+                            <Link href="/sign-up">
+                                <Button className="bg-pink-400 hover:bg-pink-800 hover:font-bold">
+                                    Start for free
+                                </Button>
+                            </Link>
+                        </>
+                    )}
                 </div>
 
             </div>
