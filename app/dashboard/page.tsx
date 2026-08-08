5acf29cc0374845f6a4d539bdd231df46747de69
oochitminn99-crypto <oochitminn99@gmail.com>
2026-08-08 21:44:54 +0630

    updated dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,37 @@
+import { getSession } from "@/lib/auth/auth";
+import connectDB from "@/lib/db";
+import { Board } from "@/lib/models";
+import { redirect } from "next/navigation";
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+    const session = await getSession();
+
+    if(!session?.user) {
+        redirect("/sign-in");
+    }
+
+    await connectDB()
+
+    const board = await Board.findOne({
+        userId: session.user.id,
+        name: "Job Hunt",
+    });
+
+    console.log(board);
+
     return (
-        <div>
-            Dashboard Page
+        <div className="min-h-screen bg-white">
+            <div className="container mx-auto p-6">
+                <div className="mb-6">
+                    <h1 className="text-3xl font-bold text-fuchsia-600">
+                        Job Hunt
+                    </h1>
+                    <p className="text-fuchsia-600">
+                        Track your job application
+                    </p>
+                </div>
+            </div>
         </div>
     )
+    
 }
