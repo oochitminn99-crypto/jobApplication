716225f56391fd77862951cb2db8ad3f1ffbfcac
oochitminn99-crypto <oochitminn99@gmail.com>
2026-08-08 21:18:53 +0630

    proxy6

--- proxy.ts
+++ proxy.ts
@@ -2,17 +2,14 @@ import { NextRequest, NextResponse } from "next/server";
 import { getSession } from "./lib/auth/auth";
 
 export default async function proxy(request: NextRequest) {
-    const session = await getSession();
+  const session = await getSession();
 
-    const isDashboardPage = request.nextUrl.pathname.startsWith("/dashboard");
+  const isSignInPage = request.nextUrl.pathname.startsWith("/sign-in");
+  const isSignUpPage = request.nextUrl.pathname.startsWith("/sign-up");
 
-    if(isDashboardPage && !session?.user) {
-        return NextResponse.redirect(new URL("/sign-in", request.url));
-    }
+  if ((isSignInPage || isSignUpPage) && session?.user) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
 
-    return NextResponse.next();
-}
-
-export const config = {
-    matcher: "/dashboard/:path*",
+  return NextResponse.next();
 }
