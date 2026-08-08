e8154611edcd01d24b455625db897b3241f9885b
oochitminn99-crypto <oochitminn99@gmail.com>
2026-08-08 15:23:53 +0630

    updated board

--- lib/init-user-board.ts
+++ lib/init-user-board.ts
@@ -1,53 +1,53 @@
 import connectDB from "./db";
-import { Board, Column, jobApplication } from "./models";
+import { Board, Column } from "./models";
 
 const DEFAULT_COLUMNS = [
-    {
-        name: "Wish List",
-        order: 0,
-    },
-    { name: "Applied", order: 1},
-    { name: "Interviewing", order: 2},
-    { name: "Offer", order: 3},
-    { name: "Rejected", order: 4},
+  {
+    name: "Wish List",
+    order: 0,
+  },
+  { name: "Applied", order: 1 },
+  { name: "Interviewing", order: 2 },
+  { name: "Offer", order: 3 },
+  { name: "Rejected", order: 4 },
 ];
 
 export async function initializeUserBoard(userId: string) {
-    try {
-        await connectDB()
+  try {
+    await connectDB();
 
-        //Check if board already exists
+    // Check if board already exists
+    const existingBoard = await Board.findOne({ userId, name: "Job Hunt" });
 
-        const existingBoard = Board.findOne({userId, name: "Job Hunt"});
+    if (existingBoard) {
+      return existingBoard;
+    }
 
-        if(existingBoard) {
-            return existingBoard;
-        }
+    // Create the board
+    const board = await Board.create({
+      name: "Job Hunt",
+      userId,
+      columns: [],
+    });
 
-        //create the board
-        const board = await Board.create({
-            name: "Job Hunt",
-            userId,
-            columns: [],
-        });
-
-        //create default columns
-        const columns = await Promise.all(DEFAULT_COLUMNS.map((col) => Column.create({
-            name: col.name,
-            order: col.order,
-            boardId: board._id,
-            jobApplication: [],
+    // Create default columns
+    const columns = await Promise.all(
+      DEFAULT_COLUMNS.map((col) =>
+        Column.create({
+          name: col.name,
+          order: col.order,
+          boardId: board._id,
+          jobApplication: [],
         })
-    )
-);
+      )
+    );
 
-    //Updated the board with the new column IDs
+    // Update the board with the new column IDs
     board.columns = columns.map((col) => col._id);
     await board.save();
 
     return board;
-
-    } catch (err) {
-        throw err;
-    }
+  } catch (err) {
+    throw err;
+  }
 }
--- lib/models/index.ts
+++ lib/models/index.ts
@@ -1,7 +1,7 @@
-import "./board"
-import "./column"
-import "./job-application"
+import "./board";
+import "./column";
+import "./job-application";
 
 export { default as Board } from "./board";
 export { default as Column } from "./column";
-export { default as jobApplication } from "./job-application";
+export { default as JobApplication } from "./job-application";
