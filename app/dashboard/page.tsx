f89943b085a34c1c7022b4c1e43997f42f9f61df
oochitminn99-crypto <oochitminn99@gmail.com>
2026-08-10 10:08:13 +0630

    change5

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,7 +6,7 @@ import KanbanBoard from "@/components/kanban-board";
 import { Suspense } from "react";
 
 async function getBoard(userId: string) {
- 'use cache'
+
   await connectDB();
 
   const boardDoc = await Board.findOne({
